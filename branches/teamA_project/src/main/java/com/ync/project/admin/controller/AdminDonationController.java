f287d144c5e7a7896ac01855d54b53d2d7507915
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-23 02:20:06 +0000

    #석준영	컨트롤러 주소변경 및 태그추가

--- branches/teamA_project/src/main/java/com/ync/project/admin/controller/AdminDonationController.java
+++ branches/teamA_project/src/main/java/com/ync/project/admin/controller/AdminDonationController.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.log4j.Log4j;
-
+/**
+ * @FileName	: HomeController.java
+ * @Date		: 2019. 10. 23. 
+ * @Author		: 석준영
+ * @프로그램 설명 : admin 페이지 후원 controller
+ */
 @Controller
 @Log4j
 public class AdminDonationController {
