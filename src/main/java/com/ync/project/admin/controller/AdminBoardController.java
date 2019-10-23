7b21090ac6d76a4f8faef7139c36ee60a590bb96
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-23 02:41:53 +0000

    #석준영	경로 admin 추가 및 게시판 controller 추가

---
+++ src/main/java/com/ync/project/admin/controller/AdminBoardController.java
@@ -0,0 +1,26 @@
+package com.ync.project.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.log4j.Log4j;
+
+/**
+ * @FileName	: HomeController.java
+ * @Date		: 2019. 10. 23. 
+ * @Author		: 석준영
+ * @프로그램 설명 : admin 페이지 게시판 controller
+ */
+@Controller
+@Log4j
+@RequestMapping("/admin/*")
+public class AdminBoardController {
+	@GetMapping(value = "/ccontent")
+//	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String donation_detail() {
+
+		log.info("Welcome Creator Content!");
+	
+		return "admin/CContent";
+	}
+}
--- src/main/java/com/ync/project/admin/controller/AdminHomeController.java
+++ src/main/java/com/ync/project/admin/controller/AdminHomeController.java
@@ -1,8 +1,8 @@
 package com.ync.project.admin.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
