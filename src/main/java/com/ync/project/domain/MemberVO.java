a03d6517063ed62271e8dc4b7dfbe1e8e555e08e
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-23 07:36:11 +0000

    #허민 userid, userpw로 변경

--- src/main/java/com/ync/project/domain/MemberVO.java
+++ src/main/java/com/ync/project/domain/MemberVO.java
@@ -16,9 +16,9 @@ public class MemberVO {
 	// userid, userpw 이름은 그대로 사용하고 나머지는 프로젝트별 생성한 table 컬럼명에 맞춰 작성
 	// spring security 적용으로 로그인 시 소스를 교재 소스 그대로 사용하기 위해.
 	
-	private String member_email; //아이디(이메일)
+	private String userid; //아이디(이메일)
 	private String name; //사용자 이름
-	private String password; //비밀번호
+	private String userpw; //비밀번호
 	private String birth; //생년월일
 	private String phone; //전화번호
 	private String level; //사용자 레벨(이용자/게임개발자)
