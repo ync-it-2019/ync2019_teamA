381e69a28fd2e1f76d73f3935491f85c5a72edb5
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-26 09:29:39 +0000

    3석준영	패키지 이름수정

--- branches/teamA_project/src/test/java/com/ync/project/front/mapper/ContentMapperTests.java
+++ branches/teamA_project/src/test/java/com/ync/project/front/mapper/ContentMapperTests.java
@@ -1,4 +1,4 @@
-package com.ync.project.mapper;
+package com.ync.project.front.mapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
