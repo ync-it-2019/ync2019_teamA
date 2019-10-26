3d6f35764d3d031caba808699af159836e155a28
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-26 07:47:00 +0000

    #김길재 AdminHomeController 링크 수정

--- src/main/java/com/ync/project/admin/controller/AdminHomeController.java
+++ src/main/java/com/ync/project/admin/controller/AdminHomeController.java
@@ -7,29 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.log4j.Log4j;
 
 /**
-  * @FileName	: HomeController.java
-  * @Date		: 2019. 10. 16. 
-  * @Author		: 정진우
+  * @FileName	: AdminHomeController.java
+  * @Date		: 2019. 10. 26. 
+  * @Author		: 김길재
   * @프로그램 설명 : 메인 페이지 호출용 controller
   */
 @Controller
 @Log4j
+@RequestMapping("/admin/*")
 public class AdminHomeController {
 	
-	/**
-	  * @Method 설명 : admin/index.jsp 호출
-	  * @Method Name : home
-	  * @Date : 2019. 10. 16.
-	  * @작성자 : 정진우
-	  * @return call jsp view
-	  */
-	@GetMapping(value = "/admin")
+	
+	@GetMapping(value = "/main")
 //	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String home() {
 
 		log.info("Welcome Admin home!");
 	
-		return "admin/index";
+		return "admin/admin_main";
 	}
 
 }
