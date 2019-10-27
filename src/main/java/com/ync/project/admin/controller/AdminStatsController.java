ca4ca7c37704e5dbad099b07f04737e70c6efcae
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-27 05:19:28 +0000

    #김길재 Admin 공지사항 Controller 추가

--- src/main/java/com/ync/project/admin/controller/AdminStatsController.java
+++ src/main/java/com/ync/project/admin/controller/AdminStatsController.java
@@ -6,11 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
+/**
+  * @FileName	: AdminStatsController.java
+  * @Date		: 2019. 10. 27. 
+  * @Author		: 김길재
+  * @프로그램 설명 : Admin 회원 통계 Controller
+  */
 @Controller
 @Log4j
 @RequestMapping("/admin/*")
 public class AdminStatsController {
 
+	 /**
+	  * @Method 설명 : 회원 통계 member_stats_all.jsp 호출
+	  * @Method Name : stats_all
+	  * @Date : 2019. 10. 27.
+	  * @작성자 : 김길재
+	  * @return
+	  */
 	@GetMapping(value = "/statsall")
 	public String stats_all() {
 
