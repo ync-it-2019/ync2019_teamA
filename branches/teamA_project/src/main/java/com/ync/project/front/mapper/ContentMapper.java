ada22ecbb6eace8f002dfd172c445b07e74990fd
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 00:50:02 +0000

    #석준영  junit오류 수정(@select 문 삭제)

--- branches/teamA_project/src/main/java/com/ync/project/front/mapper/ContentMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/mapper/ContentMapper.java
@@ -2,15 +2,10 @@ package com.ync.project.front.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
-
 import com.ync.project.domain.ContentVO;
 
 public interface ContentMapper {
-	
-	@Select("SELECT		* "
-			+ "FROM		teamACreaterContent "
-			+ "WHERE	content_id > 0")
+
 	public List<ContentVO> getList();
 
 	public int update(ContentVO content);
