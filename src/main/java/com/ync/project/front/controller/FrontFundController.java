d5584ae139909201f6f29d9b870c6cbd0fac5fe3
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 03:22:03 +0000

    #허민 프론트 컨트롤러 전체적 수정

--- src/main/java/com/ync/project/front/controller/FrontFundController.java
+++ src/main/java/com/ync/project/front/controller/FrontFundController.java
@@ -2,6 +2,7 @@ package com.ync.project.front.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,6 +17,7 @@ import lombok.extern.log4j.Log4j;
   */
 @Controller
 @Log4j
+@RequestMapping("/fund/*")
 public class FrontFundController {
 	
 	/**
