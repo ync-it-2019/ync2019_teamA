2e04c028e1b448e93d9a4f71879995f37fb15737
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 02:46:33 +0000

    @김정현 즐겨찾기(bookmark) 테스트파일 추가

---
+++ src/test/java/com/ync/project/front/mapper/BookmarkMapperTests.java
@@ -0,0 +1,29 @@
+package com.ync.project.front.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+// Java Config
+// @ContextConfiguration(classes = {org.zerock.config.RootConfig.class} )
+@Log4j
+public class BookmarkMapperTests {
+	@Autowired
+	private BookmarkMapper mapper;
+	
+	@Test
+	public void testGetList() {
+		
+		mapper.getList().forEach((Bookmark) -> {
+			log.info("==================");
+			log.info(Bookmark);
+			});
+
+	}
+}
