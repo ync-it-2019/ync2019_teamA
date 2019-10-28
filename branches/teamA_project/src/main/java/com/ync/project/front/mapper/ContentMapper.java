547b8575c4f8d5c6e32fa47324ae94b679fdcc4c
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 01:28:11 +0000

    #석준영  매퍼 재수정

--- branches/teamA_project/src/main/java/com/ync/project/front/mapper/ContentMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/mapper/ContentMapper.java
@@ -2,18 +2,36 @@ package com.ync.project.front.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.ync.project.domain.ContentVO;
+import com.ync.project.domain.Criteria;
 
+/**
+  * @FileName	: BoardMapper.java
+  * @Date		: 2019. 10. 26. 
+  * @Author		: 김정현
+  * @프로그램 설명 :  게시판 Mapper interface
+  */
 public interface ContentMapper {
+		public List<ContentVO> getList(); //게시글 목록
 
-	public List<ContentVO> getList();
+		public List<ContentVO> getListWithPaging(Criteria cri); //게시글 페이징
 
-	public int update(ContentVO content);
+		public void insert(ContentVO content); //게시글 추가
 
-	public void insertSelectKey(ContentVO content);
+		public Integer insertSelectKey(ContentVO content); //게시글 번호
 
-	public ContentVO read(Long content_id);
+		public ContentVO read(Long content_id); //게시글 내용
 
-	public int delete(Long content_id);
+		public int delete(Long content_id); //게시글 삭제
 
-}
+		public int update(ContentVO content); //게시글 수정
+
+		public int getTotalCount(Criteria cri); //게시글 전체 수
+
+		// 2개 이상의 파라미터를 넘기기위해 @Param 사용. 댓글 추가/삭제 시 amount에 1/-1 값
+		public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
+	}
+
