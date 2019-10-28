1541b6a89f7d7064f1ada0507cc5ff3f6c230c17
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 05:15:43 +0000

    #김정현 즐겨찾기(북마크) testread, testdelete 추가

--- branches/teamA_project/src/test/java/com/ync/project/front/mapper/BookmarkMapperTests.java
+++ branches/teamA_project/src/test/java/com/ync/project/front/mapper/BookmarkMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ync.project.domain.BookmarkVO;
+
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,4 +28,23 @@ public class BookmarkMapperTests {
 			});
 
 	}
+	
+	@Test
+	public void testRead() {
+
+		// 존재하는 게시물 번호로 테스트
+		BookmarkVO content_id = mapper.read(1L);
+
+		log.info(content_id);
+
+	}
+	
+	@Test
+	public void testDelete() {
+
+		log.info("DELETE COUNT: " + mapper.delete(1L));
+	}
+	
+	
+
 }
