2f35ed8418c2c20954b22fdd7524c4a8005123da
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 11:11:57 +0000

    #석준영  테스트 파일 수정

--- branches/teamA_project/src/test/java/com/ync/project/admin/mapper/MemberMapperTests.java
+++ branches/teamA_project/src/test/java/com/ync/project/admin/mapper/MemberMapperTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ync.project.domain.MemberVO;
+import com.ync.project.front.mapper.MemberMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
