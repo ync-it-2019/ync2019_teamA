a17641debe08429fe7e6e78ca167f50913aca558
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 07:29:44 +0000

    #김정현 EventMapper 추가

--- branches/teamA_project/src/main/java/com/ync/project/front/mapper/EventMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/mapper/EventMapper.java
@@ -2,8 +2,15 @@ package com.ync.project.front.mapper;
 
 import java.util.List;
 
+
 import com.ync.project.domain.EventVO;
 
+/**
+  * @FileName	: EventMapper.java
+  * @Date		: 2019. 10. 28. 
+  * @Author		: 김정현
+  * @프로그램 설명 : 이벤트 Mapper Interface
+  */
 public interface EventMapper {
 	
 	public List<EventVO> getList(); //이벤트 목록 불러오기
