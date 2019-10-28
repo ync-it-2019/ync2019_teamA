0db966691ff60aeeaf53bd27ec52acd15e037d05
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 06:29:33 +0000

    #김정현 고객센터 Mapper 추가

--- branches/teamA_project/src/main/java/com/ync/project/front/mapper/InquiryMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/mapper/InquiryMapper.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 import com.ync.project.domain.InquiryVO;
 
+
+/**
+  * @FileName	: InquiryMapper.java
+  * @Date		: 2019. 10. 28. 
+  * @Author		: 김정현
+  * @프로그램 설명 : 고객센터 정보 관리
+  */
 public interface InquiryMapper {
 	
 	public List<InquiryVO> getList(); //고객센터 목록 불러오기
