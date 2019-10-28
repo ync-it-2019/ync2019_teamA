10598813a917e5575cff26af8458653435cea16b
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 02:58:54 +0000

    #허민 게임컨텐츠컨트롤러 수정

--- branches/teamA_project/src/main/java/com/ync/project/front/controller/FrontGameContentController.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/controller/FrontGameContentController.java
@@ -7,13 +7,19 @@ import lombok.extern.log4j.Log4j;
 
 
 
+ /**
+  * @FileName	: FrontGameContentController.java
+  * @Date		: 2019. 10. 28. 
+  * @Author		: 허 민
+  * @프로그램 설명 : front 게임 게시판 관련 Controller
+  */
 @Controller
 @Log4j
 public class FrontGameContentController {
 	
 	 /**
-	  * @Method 설명 : 개발노트 front/developer_note 호출
-	  * @Method Name : developer_note
+	  * @Method 설명 : 게임 상세보기 front/game_content 호출
+	  * @Method Name : gameContent
 	  * @Date : 2019. 10. 28.
 	  * @작성자 : 허 민
 	  * @return
@@ -26,9 +32,10 @@ public class FrontGameContentController {
 		return "front/game_content";
 	}
 	
+
 	 /**
-	  * @Method 설명 : 개발노트 상세보기 front/game_developer_note 호출
-	  * @Method Name : game_developer_note
+	  * @Method 설명 : 컨텐츠 글쓰기 폼 front/game_content_writeform 호출
+	  * @Method Name : gameContentWriteForm
 	  * @Date : 2019. 10. 28.
 	  * @작성자 : 허 민
 	  * @return
@@ -41,12 +48,34 @@ public class FrontGameContentController {
 		return "front/game_content_writeform";
 	}
 	
+	 /**
+	  * @Method 설명 : 게임 소개 front/game_intro 호출
+	  * @Method Name : gameIntro
+	  * @Date : 2019. 10. 28.
+	  * @작성자 : 허 민
+	  * @return
+	  */
 	@GetMapping(value = "/game_intro")
 	public String gameIntro() {
 
 		log.info("gameintro!");
 
 		return "front/game_intro";
+	}
+	
+	 /**
+	  * @Method 설명 : 게임 리스트 front/game_list 호출
+	  * @Method Name : gameList
+	  * @Date : 2019. 10. 28.
+	  * @작성자 : 허 민
+	  * @return
+	  */
+	@GetMapping(value = "/game_intro")
+	public String gameList() {
+
+		log.info("gamelist!");
+
+		return "front/game_list";
 	}
 	
 	
