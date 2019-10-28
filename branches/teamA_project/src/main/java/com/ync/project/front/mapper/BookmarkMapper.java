fa9a98122b579579157b31d3777a1790f2e5fca2
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-28 05:13:55 +0000

    #북마크 인터페이스 수정

--- branches/teamA_project/src/main/java/com/ync/project/front/mapper/BookmarkMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/mapper/BookmarkMapper.java
@@ -2,11 +2,18 @@ package com.ync.project.front.mapper;
 
 import java.util.List;
 
+import com.ync.project.domain.BookmarkVO;
 import com.ync.project.domain.ContentVO;
 
+ /**
+  * @FileName	: BookmarkMapper.java
+  * @Date		: 2019. 10. 28. 
+  * @Author		: 허 민
+  * @프로그램 설명 : 북마크 인터페이스
+  */
 public interface BookmarkMapper {
-	public List<ContentVO> getList(); //북마크 목록 불러오기
+	public List<BookmarkVO> getList(); //북마크 목록 불러오기
 	
-	public int delete(Long content_id); //북마크 해제하기
+	public int delete(Long bookmark); //북마크 해제하기
 	
 }
