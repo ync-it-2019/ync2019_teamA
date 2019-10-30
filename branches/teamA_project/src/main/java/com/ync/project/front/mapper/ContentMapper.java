9ce132aa641535d70d16dbdd113e2291b6204d1f
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-30 12:21:28 +0000

    #석준영  작은수정

--- branches/teamA_project/src/main/java/com/ync/project/front/mapper/ContentMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/mapper/ContentMapper.java
@@ -3,7 +3,6 @@ package com.ync.project.front.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import com.ync.project.domain.ContentVO;
 import com.ync.project.domain.Criteria;
