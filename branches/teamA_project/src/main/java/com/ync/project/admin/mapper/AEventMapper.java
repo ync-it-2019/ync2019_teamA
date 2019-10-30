cca524acc4d37f476750155431d27b01551ba006
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-30 01:50:31 +0000

    #심재민	어드민 이벤트 맵퍼 java파일 커밋

--- branches/teamA_project/src/main/java/com/ync/project/admin/mapper/AEventMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/admin/mapper/AEventMapper.java
@@ -2,8 +2,9 @@ package com.ync.project.admin.mapper;
 
 import java.util.List;
 
-
+import com.ync.project.domain.Criteria;
 import com.ync.project.domain.EventVO;
+import com.ync.project.domain.NoticeVO;
 
 /**
   * @FileName	: EventMapper.java
@@ -13,11 +14,17 @@ import com.ync.project.domain.EventVO;
   */
 public interface AEventMapper {
 	
-	public List<EventVO> getList(); 		//이벤트 목록 불러오기
+	public List<EventVO> getList(); 						// 이벤트 목록 불러오기
 	
-	public EventVO read(Long event_id); 	//이벤트 상세보기
+	public EventVO read(Long event_id); 					// 이벤트 상세보기
 	
-	public int delete(Long event_id); 		//이벤트 삭제하기
+	public int delete(Long event_id); 						// 이벤트 삭제하기
 	
-	public void insert(EventVO event);		// 이벤트글 삽입
+	public void insert(EventVO event);						// 이벤트글 삽입
+
+	public Integer insertSelectKey(EventVO notice); 		// 게시글 번호
+
+	public List<EventVO> getListWithPaging(Criteria cri);	// 이벤트 글 페이징
+
+	public int getTotalCount(Criteria cri);					// 전체 이벤트글
 }
