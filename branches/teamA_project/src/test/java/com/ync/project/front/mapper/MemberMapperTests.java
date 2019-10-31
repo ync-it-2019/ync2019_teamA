83530e89fc4b75ed7b7e53df6d5139bf6e4d793d
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-10-31 08:31:15 +0000

    #김정현 프론트 member mapper 파일

--- branches/teamA_project/src/test/java/com/ync/project/front/mapper/MemberMapperTests.java
+++ branches/teamA_project/src/test/java/com/ync/project/front/mapper/MemberMapperTests.java
@@ -19,9 +19,30 @@ public class MemberMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private MemberMapper mapper;
 	
+//	@Test
+//	public void testRead() { //tjrwnsdud2에 대한 정보를 조회 MemberVO와 AuthvO가 구성됨
+//		
+//		MemberVO vo = mapper.read("tjrwnsdud2@naver.com");
+//	}
+//	
 	@Test
-	public void testRead() { //tjrwnsdud2에 대한 정보를 조회 MemberVO와 AuthvO가 구성됨
+	public void testInsert() { 
 		
-		MemberVO vo = mapper.read("tjrwnsdud2@naver.com");
+		MemberVO vo = new MemberVO();
+		vo.setUserid("hyeon@mail.com");
+		vo.setName("jeonghyeon");
+		vo.setBirth("960202");
+		vo.setPhone("01039290412");
+		vo.setUserpw("1234");
+		vo.setCreater_name("another");
+		vo.setBank_account(null);
+		vo.setDonation_accept("1");
+		vo.setLevels("1");
+		vo.setStatus("1");
+		vo.setSns(null);
+		vo.setOther_address(null);
+		
+		mapper.insert(vo);
+		log.info(vo);
 	}
 }
