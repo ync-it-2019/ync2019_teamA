8b07516ec25996c2060ff45d04769d2555e356f0
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-04 05:07:43 +0000

    #심재민	1:1문의 페이지 리스트 보기 완성. 		상세보기페이지 미구현

--- branches/teamA_project/src/main/java/com/ync/project/admin/controller/AdminInquiryController.java
+++ branches/teamA_project/src/main/java/com/ync/project/admin/controller/AdminInquiryController.java
@@ -1,9 +1,15 @@
 package com.ync.project.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ync.project.admin.service.AInquiryService;
+import com.ync.project.domain.Criteria;
+import com.ync.project.domain.PageDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -11,16 +17,25 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/admin/*")
 public class AdminInquiryController {
-	
+
+	@Autowired
+	private AInquiryService iService;
 
 	@Value("${globalConfig.uploadPath}")
 	private String uploadPath;
 	
 	@GetMapping(value = "/inquiry_qna_list")
 	//@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public String qna_list() {
+	public String qna_list(Criteria cri, Model model) {
 
-		log.info("Welcome Content Acontent!");
+		int total = iService.getTotal(cri);
+
+		log.info("Welcome Content Management!");
+		log.info("Content Lists......\n" + cri + "\n#<--//end Content Lists +-+-+-+-+-+-+-+-+-+-+-//-->");
+		log.info("Total Value = " + total + "\n#<--//end Total Value +-+-+-+-+-+-+-+-+-+-+-//-->");
+		
+		model.addAttribute("inquiry_list", iService.getListWithPaging(cri));
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
 	
 		return "admin/inquiry_qna_list";
 	}
