42014d6aa487c93a03f1d14cc4b8012900401d88
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-09 08:37:02 +0000

    #석준영  패치노트 파일 업로드

--- branches/teamA_project/src/main/java/com/ync/project/front/controller/FrontDevelNoteController.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/controller/FrontDevelNoteController.java
@@ -2,8 +2,15 @@ package com.ync.project.front.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ync.project.domain.Criteria;
+import com.ync.project.domain.PageDTO;
+import com.ync.project.front.service.ChangelogService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -11,9 +18,12 @@ import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("/front/*")
+@RequestMapping("/*")
 public class FrontDevelNoteController {
+	
 	@Autowired
+	private ChangelogService service;
+	
 
 	 /**
 	  * @Method 설명 : 개발노트 front/developer_note 호출
@@ -23,10 +33,16 @@ public class FrontDevelNoteController {
 	  * @return
 	  */
 	@GetMapping(value = "/developer_note")
-	public void developerNote() {
-
+	public String developerNote(Criteria cri, Model model) {
+		
 		log.info("develnote!");
-
+		int total = service.getTotal(cri);
+		log.info("total: " + total);
+		
+		model.addAttribute("chlog_list", service.getListWithPaging(cri));
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+		
+		return "/front/developer_note";
 	}
 	
 	 /**
@@ -37,10 +53,15 @@ public class FrontDevelNoteController {
 	  * @return
 	  */
 	@GetMapping(value = "/game_developer_note")
-	public void gameDeveloperNote() {
-
+	public String gameDeveloperNote(@RequestParam("change_log_id") Long change_log_id, @ModelAttribute("cri") Criteria cri, Model model) {
+		
 		log.info("develnote detail!");
-
+		log.info("/get............." + change_log_id);
+		model.addAttribute("change_log_content", service.get(change_log_id));
+		model.addAttribute("changelog_comment", service.getchangelog_comment(change_log_id));
+		model.addAttribute("patch_note_List", service.getpatch_note(change_log_id));
+		model.addAttribute("other_patch", service.getother_patch(change_log_id));
+		return "/front/game_developer_note";
 	}
 	
 }
