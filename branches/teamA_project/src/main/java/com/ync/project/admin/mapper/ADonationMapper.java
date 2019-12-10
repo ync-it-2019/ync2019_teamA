14da1ca560489ba02d982f48581c343cde563654
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-10 03:46:19 +0000

    @김정현 후원관리 매퍼

--- branches/teamA_project/src/main/java/com/ync/project/admin/mapper/ADonationMapper.java
+++ branches/teamA_project/src/main/java/com/ync/project/admin/mapper/ADonationMapper.java
@@ -3,8 +3,8 @@ package com.ync.project.admin.mapper;
 import java.util.List;
 
 import com.ync.project.domain.Criteria;
-import com.ync.project.domain.GDonationVO;
 import com.ync.project.domain.DonationVO;
+import com.ync.project.domain.GDonationVO;
 
 
 //복사 붙여넣기 후 이름만 수정하였음
@@ -35,6 +35,10 @@ public interface ADonationMapper {
 	public int getTotalDonation();								//후원한 총 금액
 	
 	public int creatersGetDonation();
+	
+	public List<DonationVO> getListWithPagingAndContent(Criteria cri); //컨텐츠id 와 조인 한 리스트
+	
+
 	
 	// 2개 이상의 파라미터를 넘기기위해 @Param 사용. 댓글 추가/삭제 시 amount에 1/-1 값
 //	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
