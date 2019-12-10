ecd754d52b52568642bbba31d53513f1a002c87f
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-10 04:54:13 +0000

    #김길재 창작자 강등, 휴면 여부 controller 추가

--- src/main/java/com/ync/project/admin/controller/AdminCreaterController.java
+++ src/main/java/com/ync/project/admin/controller/AdminCreaterController.java
@@ -3,13 +3,18 @@ package com.ync.project.admin.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ync.project.admin.service.AMemberService;
 import com.ync.project.domain.Criteria;
+import com.ync.project.domain.MemberVO;
 import com.ync.project.domain.PageDTO;
 
 import lombok.extern.log4j.Log4j;
@@ -18,7 +23,7 @@ import lombok.extern.log4j.Log4j;
  * @FileName	: AdminCreatorController.java
  * @Date		: 2019. 10. 23. 
  * @Author		: 석준영
- * @프로그램 설명 : admin 후원 게시판 controller
+ * @프로그램 설명 : admin 창작자 관리 controller
  */
 @Controller
 @Log4j
@@ -34,8 +39,8 @@ public class AdminCreaterController {
 	  * @작성자 : 석준영
 	  * @return
 	  */
-	@GetMapping(value = "/creater_modify")
-	public void creater_modify(HttpServletRequest request, Model model, Criteria cri) {
+	@GetMapping(value = "/creator_modify")
+	public void creator_modify( HttpServletRequest request, Model model, Criteria cri) {
 
 		String userid = request.getParameter("userid");
 		
@@ -44,7 +49,53 @@ public class AdminCreaterController {
 		model.addAttribute("member_info", mService.get(userid));
 		model.addAttribute("givedon", mService.getGiveDonation(cri));
 		model.addAttribute("getdon", mService.getGetDonationToUser(userid));
+
 	}
+	
+	 /**
+	  * @Method 설명 : 창작자 권한 강등
+	  * @Method Name : creator_modifying
+	  * @Date : 2019. 12. 10.
+	  * @작성자 : 김길재
+	  * @param userid
+	  * @param request
+	  * @param model
+	  * @param cri
+	  * @return
+	  */
+	@GetMapping(value = "/creator_modifying")
+	public String creator_modifying(@RequestParam("userid") String userid, HttpServletRequest request, Model model, Criteria cri) {
+
+		
+		log.info("Welcome Info detail!");
+		
+		mService.creator_modify(userid);
+		
+		return "redirect:/admin/creator_info";
+	}
+	
+	 /**
+	  * @Method 설명 : 창작자 휴면 전환
+	  * @Method Name : creator_status
+	  * @Date : 2019. 12. 10.
+	  * @작성자 : 김길재
+	  * @param userid
+	  * @param request
+	  * @param model
+	  * @param cri
+	  * @return
+	  */
+	@GetMapping(value = "/creator_status")
+	public String creator_status(@RequestParam("userid") String userid, HttpServletRequest request, Model model, Criteria cri) {
+
+		
+		log.info("Welcome Info detail!");
+		
+		mService.creator_status(userid);
+		
+		return "redirect:/admin/creator_info";
+	}
+	
 	
 	 /**
 	  * @Method 설명 : 창작자 조회 Creator_list.jsp 호출
@@ -53,8 +104,8 @@ public class AdminCreaterController {
 	  * @작성자 : 김길재
 	  * @return
 	  */
-	@GetMapping(value = "/creater_info")
-	public void creater_info(Criteria cri, Model model) {
+	@GetMapping(value = "/creator_info")
+	public void creator_info(Criteria cri, Model model) {
 		int total = mService.getTotalCreater(cri);
 		
 		log.info("list:11111 " + cri);
