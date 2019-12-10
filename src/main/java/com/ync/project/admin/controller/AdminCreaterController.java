393d7ce407d25258d8faa17ae375c13129ae292a
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-10 05:18:50 +0000

    #김길재 창작자 강등, 휴면 복귀 controller 추가

--- src/main/java/com/ync/project/admin/controller/AdminCreaterController.java
+++ src/main/java/com/ync/project/admin/controller/AdminCreaterController.java
@@ -3,24 +3,20 @@ package com.ync.project.admin.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ync.project.admin.service.AMemberService;
 import com.ync.project.domain.Criteria;
-import com.ync.project.domain.MemberVO;
 import com.ync.project.domain.PageDTO;
 
 import lombok.extern.log4j.Log4j;
 
 /**
- * @FileName	: AdminCreatorController.java
+ * @FileName	: AdmincreaterController.java
  * @Date		: 2019. 10. 23. 
  * @Author		: 석준영
  * @프로그램 설명 : admin 창작자 관리 controller
@@ -33,14 +29,14 @@ public class AdminCreaterController {
 	@Autowired
 	private AMemberService mService;
 	 /**
-	  * @Method 설명 : 창작자 권한 수정 admin_generate_creator.jsp 호출
-	  * @Method Name : creatorGenerate
+	  * @Method 설명 : 창작자 권한 수정 admin_generate_creater.jsp 호출
+	  * @Method Name : createrGenerate
 	  * @Date : 2019. 10. 26.
 	  * @작성자 : 석준영
 	  * @return
 	  */
-	@GetMapping(value = "/creator_modify")
-	public void creator_modify( HttpServletRequest request, Model model, Criteria cri) {
+	@GetMapping(value = "/creater_modify")
+	public void creater_modify( HttpServletRequest request, Model model, Criteria cri) {
 
 		String userid = request.getParameter("userid");
 		
@@ -54,7 +50,7 @@ public class AdminCreaterController {
 	
 	 /**
 	  * @Method 설명 : 창작자 권한 강등
-	  * @Method Name : creator_modifying
+	  * @Method Name : creater_modifying
 	  * @Date : 2019. 12. 10.
 	  * @작성자 : 김길재
 	  * @param userid
@@ -63,20 +59,20 @@ public class AdminCreaterController {
 	  * @param cri
 	  * @return
 	  */
-	@GetMapping(value = "/creator_modifying")
-	public String creator_modifying(@RequestParam("userid") String userid, HttpServletRequest request, Model model, Criteria cri) {
+	@GetMapping(value = "/creater_modifying")
+	public String creater_modifying(@RequestParam("userid") String userid, HttpServletRequest request, Model model, Criteria cri) {
 
 		
 		log.info("Welcome Info detail!");
 		
-		mService.creator_modify(userid);
+		mService.creater_modify(userid);
 		
-		return "redirect:/admin/creator_info";
+		return "redirect:/admin/creater_info";
 	}
 	
 	 /**
 	  * @Method 설명 : 창작자 휴면 전환
-	  * @Method Name : creator_status
+	  * @Method Name : creater_status
 	  * @Date : 2019. 12. 10.
 	  * @작성자 : 김길재
 	  * @param userid
@@ -85,27 +81,27 @@ public class AdminCreaterController {
 	  * @param cri
 	  * @return
 	  */
-	@GetMapping(value = "/creator_status")
-	public String creator_status(@RequestParam("userid") String userid, HttpServletRequest request, Model model, Criteria cri) {
+	@GetMapping(value = "/creater_status")
+	public String creater_status(@RequestParam("userid") String userid, HttpServletRequest request, Model model, Criteria cri) {
 
 		
 		log.info("Welcome Info detail!");
 		
-		mService.creator_status(userid);
+		mService.creater_status(userid);
 		
-		return "redirect:/admin/creator_info";
+		return "redirect:/admin/creater_info";
 	}
 	
 	
 	 /**
-	  * @Method 설명 : 창작자 조회 Creator_list.jsp 호출
+	  * @Method 설명 : 창작자 조회 creater_list.jsp 호출
 	  * @Method Name : grademod
 	  * @Date : 2019. 10. 24.
 	  * @작성자 : 김길재
 	  * @return
 	  */
-	@GetMapping(value = "/creator_info")
-	public void creator_info(Criteria cri, Model model) {
+	@GetMapping(value = "/creater_info")
+	public void creater_info(Criteria cri, Model model) {
 		int total = mService.getTotalCreater(cri);
 		
 		log.info("list:11111 " + cri);
