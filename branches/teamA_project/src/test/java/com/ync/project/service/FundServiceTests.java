699ff3ddc5d0a467b5c1a0fe7f0900de254c226a
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-13 01:18:19 +0000

    #허민 펀딩상세페이지 테스트 추가

--- branches/teamA_project/src/test/java/com/ync/project/service/FundServiceTests.java
+++ branches/teamA_project/src/test/java/com/ync/project/service/FundServiceTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ync.project.domain.ContentVO;
+import com.ync.project.domain.FundVO;
 import com.ync.project.front.service.FundingService;
 
 import lombok.extern.log4j.Log4j;
@@ -26,18 +28,54 @@ public class FundServiceTests {
 	@Autowired
 	private FundingService service;
 
-	// 펀딩 메인
+//	// 펀딩 메인
+//	@Test
+//	public void testGetListMain() {
+//
+//		service.getListWithPaging().forEach(content -> log.info(content));
+//	}
+//
+//	// 펀딩 종료
+//	@Test
+//	public void testGetListEnd() {
+//
+//		service.getListEndFund().forEach(content -> log.info(content));
+//
+//	}
+	
+	//상세 페이지
+//	@Test
+//	public void testRead() {
+//
+//		// 존재하는 게시물 번호로 테스트
+//		ContentVO content = service.read(1L);
+//
+//		log.info(content);
+//
+//	}
+	
+	//추가
+//	@Test
+//	public void testInsert() {
+//		FundVO content = new FundVO();
+//		content.setContent_id(1L);
+//		content.setDonation(30000L);
+//		content.setUserid("tjrwnsdud2@naver.com");
+//		service.insert(content);	
+//
+//		log.info(content);
+//	}
+	
+	//값 변경
 	@Test
-	public void testGetListMain() {
+	public void testUpdate() throws Exception {
+		ContentVO content = new ContentVO();
+		// 실행전 존재하는 번호인지 확인할 것
+		content.setContent_id(1L);
+		content.setMoney_temp(30000L);
 
-		service.getListWithPaging().forEach(content -> log.info(content));
-	}
-
-	// 펀딩 종료
-	@Test
-	public void testGetListEnd() {
-
-		service.getListEndFund().forEach(content -> log.info(content));
+		boolean count = service.modify(content) == true;
+		log.info("UPDATE COUNT: " + count);
 
 	}
 
