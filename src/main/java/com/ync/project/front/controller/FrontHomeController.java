7b066a6119d647ce9a2ae82610e8eb71735d3803
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-13 07:33:13 +0000

    #심재민	inquiry페이지 포스트구현, notice/inquiry페이지 진입을 홈컨트롤러에서 작동하도록 변경

--- src/main/java/com/ync/project/front/controller/FrontHomeController.java
+++ src/main/java/com/ync/project/front/controller/FrontHomeController.java
@@ -5,9 +5,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ync.project.domain.InquiryVO;
 import com.ync.project.front.service.HomeService;
+import com.ync.project.front.service.InquiryService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -21,9 +24,12 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/*")
 public class FrontHomeController {
+	
 	@Autowired
 	private HomeService service;
-	
+
+	@Autowired
+	private InquiryService iService;
 	
 	 /**
 	  * @Method 설명 : 매인화면 게임 리스트 front/index 호출
@@ -44,5 +50,36 @@ public class FrontHomeController {
 		
 		return "/front/index";
 	}
+
+	@GetMapping(value = "/inquiry")
+	@PreAuthorize("isAuthenticated()")
+	public String inquiry() {
+
+		log.info("get page inquiry......");
 	
+		return "front/inquiry";
+	}
+
+	@PostMapping(value = "/inquiry")
+	public String writeInquiry(InquiryVO inquiry) {
+		
+		log.warn(inquiry.getTitle());
+		log.warn(inquiry.getUserid());
+		log.warn(inquiry.getContent());
+		
+		if(iService.insert(inquiry) == 0) {
+			log.info("Sql error");
+		}
+		log.info("write inquiry......");
+	
+		return "front/inquiry";
+	}
+	
+	@GetMapping(value = "/faq")
+	public String faq() {
+
+		log.info("faq!");
+	
+		return "front/faq";
+	}
 }
