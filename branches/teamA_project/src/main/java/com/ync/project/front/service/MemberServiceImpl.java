4ce27e960b99f7099ad5246c7154d0300e38e77f
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-13 09:41:19 +0000

    #김길재 회원 정보 보기, 수정 추가

--- branches/teamA_project/src/main/java/com/ync/project/front/service/MemberServiceImpl.java
+++ branches/teamA_project/src/main/java/com/ync/project/front/service/MemberServiceImpl.java
@@ -7,18 +7,19 @@ import com.ync.project.domain.MemberVO;
 import com.ync.project.front.mapper.MemberMapper;
 
 import lombok.extern.log4j.Log4j;
+
 /**
-  * @FileName	: MemberServiceImpl.java
-  * @Date		: 2019. 11. 25. 
-  * @Author		: 김정현
-  * @프로그램 설명 : 회원 관련 서비스 impl 파일 생성
-  */
+ * @FileName : MemberServiceImpl.java
+ * @Date : 2019. 11. 25.
+ * @Author : 김정현
+ * @프로그램 설명 : 회원 관련 서비스 impl 파일 생성
+ */
 @Log4j
 @Service
 public class MemberServiceImpl implements MemberService {
 	@Autowired
 	private MemberMapper mapper;
-	
+
 	@Override
 	public void join(MemberVO member) {
 		// TODO Auto-generated method stub
@@ -48,7 +49,7 @@ public class MemberServiceImpl implements MemberService {
 	public MemberVO get(String userid) {
 		// TODO Auto-generated method stub
 		log.info("read User informations by ......" + userid);
-		
+
 		return mapper.read(userid);
 	}
 
@@ -56,15 +57,31 @@ public class MemberServiceImpl implements MemberService {
 	public MemberVO checkPassword(MemberVO member) {
 		// TODO Auto-generated method stub
 		log.info("Check User information Service by ......" + member);
-		
+
 		return mapper.checkUser(member);
 	}
-	
+
 	@Override
 	public boolean request_creator(MemberVO r_creator) {
 		log.info("modify......" + r_creator);
 
 		return mapper.request_creator(r_creator) == 1;
 	}
+
+
+	@Override
+	public MemberVO read_member(String userid) {
+		log.info("read User informations by ......" + userid);
+
+		return mapper.read_member(userid);
+	}
+
+	@Override
+	public boolean member_modify(MemberVO userid) {
+		log.info("modify......" + userid);
+
+		return mapper.member_modify(userid) == 1;
+	}
+
 
 }
