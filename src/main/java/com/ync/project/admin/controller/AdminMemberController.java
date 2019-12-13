ea6209890be9677ed76782f6b4cd1ef901aef978
team_a <team_a@32119d58-617c-4a1b-bdcb-c55e162fc19d>
2019-12-13 06:39:00 +0000

    #심재민	어드민페이지 로그인시에만 접속 가능하도록 권한 설정 및 /admin/ 이후 경로 잘못 되면 메인으로 돌아오도록 설정

--- src/main/java/com/ync/project/admin/controller/AdminMemberController.java
+++ src/main/java/com/ync/project/admin/controller/AdminMemberController.java
@@ -3,6 +3,7 @@ package com.ync.project.admin.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,7 @@ import lombok.extern.log4j.Log4j;
 @Controller
 @Log4j
 @RequestMapping("/admin/*")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminMemberController {
 
 	@Autowired
